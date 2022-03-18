bf562e9c80b4116d0aaa2e850518123569a0e456
PrithiviRaj <vjprithivi@gmail.com>
2022-03-18 10:02:06 +0530

    initalized

---
+++ app/src/main/java/com/task/news/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.task.news
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+
+    }
+}
