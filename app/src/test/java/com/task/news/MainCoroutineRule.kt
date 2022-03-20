a77c21973bd5838fb0d37b0eff2c5e7dcf17eab6
PrithiviRaj <vjprithivi@gmail.com>
2022-03-20 23:44:00 +0530

    MVVM

--- app/src/test/java/com/task/news/MainCoroutineRule.kt
+++ app/src/test/java/com/task/news/MainCoroutineRule.kt
@@ -3,10 +3,7 @@ package com.task.news
 import com.task.news.daggers.CoroutinesDispatcherProvider
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.TestCoroutineDispatcher
-import kotlinx.coroutines.test.resetMain
-import kotlinx.coroutines.test.runBlockingTest
-import kotlinx.coroutines.test.setMain
+import kotlinx.coroutines.test.*
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
